3e4ec36bac1fb7f9ef93fe1537f9f102d4030af7
Johnny Crisp <johnnycrisp2@gmail.com>
2023-06-13 13:30:12 +0100

    add hooks and update App.js with index/layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {ThemeProvider} from "styled-components";
 import {theme} from "./utils/styling";
 import GlobalStyle from "./components/atoms/GlobalStyles";
+import './utils/fontface.css'
+import {useWindowSize} from 'react-use'
+
 
 const ClientSideOnlyLazyFleetBuilderScene = React.lazy(() =>
     import('./components/organisms/FleetBuilderScene')
 )
 function App() {
+  const size = useWindowSize();
+  useEffect(() => {
+    document.documentElement.style.setProperty('--windowHeight', `${size.height}px`)
+}, [size])
+
   const isSSR = typeof window === 'undefined'
 
   return (
